6dd620da3ddd13c5899cdb3a7f617bd9727e3e33
Yuandjom <ziyangjohn@gmail.com>
2023-01-15 21:53:55 +0800

    Explore page tbc

--- frontend/src/pages/explore/Explore.jsx
+++ frontend/src/pages/explore/Explore.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import './Explore.css'
 
-function Explore() {
+export default function Explore({posts}) {
   return (
-    <div>Explore</div>
+    <div className="customer-order">
+      <div class="customer-center">
+        <span className="customer-name">{posts.name}</span>
+      </div>
+      <hr class="customer-hr"></hr>
+      <span>
+          <div className="customer-contact">Telegram Handle: {posts.tele}</div>
+      </span>
+      <span>
+          <div className="customer-residence">Message: {posts.msg}</div>
+      </span>
+      {/* <span>
+          <div className="customer-price">Paid: {posts.price}</div>
+      </span> */}
+
+    </div>
   )
 }
 
-export default Explore
