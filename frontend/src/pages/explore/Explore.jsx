9467b7d70522f4df1573db1bf9a1b19603fcb643
Yuandjom <ziyangjohn@gmail.com>
2023-01-15 23:36:29 +0800

    post in explore page

--- frontend/src/pages/explore/Explore.jsx
+++ frontend/src/pages/explore/Explore.jsx
@@ -1,24 +1,63 @@
-import React from 'react'
 import './Explore.css'
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import ButtonBase from '@mui/material/ButtonBase';
+// import smallBoxImage from '../assets/smallBoxes.jpg'
+
+const Img = styled('img')({
+  margin: 'auto',
+  display: 'block',
+  maxWidth: '100%',
+  maxHeight: '100%',
+});
 
 export default function Explore({posts}) {
   return (
-    <div className="customer-order">
-      <div class="customer-center">
-        <span className="customer-name">{posts.name}</span>
-      </div>
-      <hr class="customer-hr"></hr>
-      <span>
-          <div className="customer-contact">Telegram Handle: {posts.tele}</div>
-      </span>
-      <span>
-          <div className="customer-residence">Message: {posts.msg}</div>
-      </span>
-      {/* <span>
-          <div className="customer-price">Paid: {posts.price}</div>
-      </span> */}
-
-    </div>
-  )
+    <Paper
+    variant="outlined"
+      sx={{
+        p: 5,
+        margin: 'auto',
+        maxWidth: 500,
+        flexGrow: 1,
+        backgroundColor: (theme) =>
+          theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+      }}
+    >
+      <Grid container spacing={3}>
+        <Grid item>
+          {/* <ButtonBase sx={{ width: 128, height: 128 }}>
+            <Img alt="small-box" src={smallBoxImage} />
+          </ButtonBase> */}
+        </Grid>
+        <Grid item xs={12} sm container>
+          <Grid item xs container direction="column" spacing={2}>
+            <Grid item xs>
+              <Typography gutterBottom variant="subtitle1" component="div">
+                {posts.name}
+              </Typography>
+              <Typography variant="body2" gutterBottom>
+                Dimensions: 35cm (L) x 25cm (W) x 25cm (H)
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+              Approximately the content of a backpack
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+              Things that fit: Documents & photos, Books & stationery, Small appliances & cookware, Toys, Laptop
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Typography variant="subtitle1" component="div">
+              $8.00
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
 }
 
