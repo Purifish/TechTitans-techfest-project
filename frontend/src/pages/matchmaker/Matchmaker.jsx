5ebbd3bdbe0be6b83b168ae8a04f288d9542b474
Purifish <purifish97@gmail.com>
2023-01-16 18:04:24 +0800

    Done with most of Matchmaker's functionalities

--- frontend/src/pages/matchmaker/Matchmaker.jsx
+++ frontend/src/pages/matchmaker/Matchmaker.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
+import { StylesManager, Model } from "survey-core";
+import { Survey } from "survey-react-ui";
+import "survey-core/defaultV2.min.css";
+
+import { quizContent } from "./quiz-content";
+
+StylesManager.applyTheme("defaultV2");
 
 function Matchmaker() {
+  // const [isDone, setDone] = useState(false);
+  const survey = new Model(quizContent);
+
+  console.log("Total questions: " + quizContent.pages.length);
+
+  survey.onComplete.add((sender, options) => {
+    console.log("Answers:\n" + JSON.stringify(sender.data, null, 3));
+    // setDone(false);
+
+    const score = 20;
+    const role = "Data analyst";
+      
+    // Save the scores in survey results
+    sender.setValue("score", score);
+    sender.setValue("role", role);
+  });
+
   return (
-    <div>Matchmaker</div>
-  )
+    <div>
+      {/* {isDone ? <h3 style={{fontSize: "50px"}}>YOOOO</h3>  : <Survey model={survey} />} */}
+      <Survey model={survey} />
+    </div>
+  );
 }
 
-export default Matchmaker
+export default Matchmaker;
---
+++ frontend/src/pages/matchmaker/quiz-content.js
@@ -0,0 +1,89 @@
+export const quizContent = { 
+    title: "Tech Career Matchmaker",
+    completedHtml: "<h2>NICE {score}. You did great {role}!</h2>",
+    pages: [
+      {
+        "title": "Which of these languages do you like the most?",
+        "elements": [
+          {
+            "type": "checkbox",
+            "name": "language",
+            "title": "Select up to 2 languages",
+            "isRequired": true,
+            "maxSelectedChoices" : 2,
+            "choices": [ "Python", "Java", "C++", "C#", "JavaScript", "Golang" ]
+          }
+        ]
+      },
+      {
+        "title": "How good are you at Mathematics?",
+        "elements": [
+          {
+            "type": "radiogroup",
+            "name": "maths",
+            "title": "Choose the option that describes you the best",
+            "isRequired": true,
+            "choices": [
+              "Very good",
+              "Above average",
+              "Average",
+              "Below average",
+              "I can't find the area of a square"
+            ]
+          }
+        ]
+      },
+      {
+        "title": "Which of these adjectives describes you the best?",
+        "elements": [
+          {
+            "type": "radiogroup",
+            "name": "thinking",
+            "title": "Select only one",
+            "isRequired": true,
+            "choices": [
+              "Logical",
+              "Artistic",
+              "Creative",
+              "Brainstormer",
+            ]
+          }
+        ]
+      },
+      {
+        "title": "Which of these interests you the most?",
+        "elements": [
+          {
+            "type": "radiogroup",
+            "name": "interest",
+            "title": "Select only one",
+            "isRequired": true,
+            "choices": [
+              "Artificial intelligence (AI)",
+              "Creating an app",
+              "Designing an app",
+              "Cracking tough coding questions (e.g. from LeetCode, HackerRank, etc)",
+              "I just love to code!"
+            ]
+          }
+        ]
+      },
+      {
+        "title": "How introverted/extroverted are you?",
+        "elements": [{
+            "type": "rating",
+            "name": "social",
+            "title": "Choose the option that describes you the best",
+            "rateValues": [
+              { "value": "social1", "text": "1" },
+              { "value": "social2", "text": "2" },
+              { "value": "social3", "text": "3" },
+              { "value": "social4", "text": "4" },
+              { "value": "social5", "text": "5" }
+            ],
+            "minRateDescription": "Highly Introverted",
+            "maxRateDescription": "Highly Extroverted"
+          }
+        ]
+      }]
+};
