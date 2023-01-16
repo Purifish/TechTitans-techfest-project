9ab632c9624e34780419da11a2f4c5dc72cb161f
BinaryAnxiety <713jiaearn@gmail.com>
2023-01-16 18:19:14 +0800

    Improved aesthetics for Skills

--- frontend/src/pages/skill/Skill.jsx
+++ frontend/src/pages/skill/Skill.jsx
@@ -7,34 +7,80 @@ function Skill() {
   const percentage = 73
   return (
     <Box padding={10}>
+      
 
       <div>
-        <Typography variant="h2" gutterBottom>Your Progress</Typography>
+        <Typography variant="h2" gutterBottom>Welcome Back John</Typography>
+      </div>
+
+
+      <div>
+        <Typography variant="h4" gutterBottom>Your Coursera Progress</Typography>
       </div>
 
       <div className="progressBar">
-        <h3>React - Coursera</h3>
-        <Typography>React skills</Typography>
+        <h4>React - Coursera</h4>
         <ProgressBar now={percentage} />
+        <Box padding={2}></Box>
       </div>
 
       <div className="progressBar">
-        <h3>NodeJS - Coursera</h3>
-        <Typography>NodeJS skills</Typography>
+        <h4>NodeJS - Coursera</h4>
+        <ProgressBar now={70} />
+        <Box padding={2}></Box>
+      </div>
+
+      <div className="progressBar">
+        <h4>AngularJS - Coursera</h4>
         <ProgressBar now={54} />
+        <Box padding={2}></Box>
+      </div>
+
+
+      <div>
+        <Typography variant="h4" gutterBottom>Your Udemy Progress</Typography>
       </div>
 
       <div className="progressBar">
-        <Typography paddingTop={1} variant='h4'>ExpressJS - Udemy</Typography>
-        <Typography paddingBottom={1} paddingTop= {1}>ExpressJS skills</Typography>
-        <ProgressBar now={40} />
+
+        <h4>ExpressJS - Udemy</h4>
+        <ProgressBar now={80} />
+        <Box padding={2}></Box>
       </div>
 
       <div className="progressBar">
-        <h3>Flask - EduX</h3>
-        <Typography>Flask Progress</Typography>
+        <h4>SQL - Udemy</h4>
+        <ProgressBar now={74} />
+        <Box padding={2}></Box>
+      </div>
+
+
+
+      <div className="progressBar">
+        <h4>Java - Udemy</h4>
+        <ProgressBar now={64} />
+        <Box padding={2}></Box>
+      </div>
+
+      <div>
+        <Typography variant="h4" gutterBottom>Your EduX Progress</Typography>
+      </div>
+
+      <div className="progressBar">
+        <h4>Flask - EduX</h4>
         <ProgressBar now={24} />
+        <Box padding={2}></Box>
       </div>
+
+      <div className="progressBar">
+        <h4>Introduction to ML - EduX</h4>
+        <ProgressBar now={10} />
+        <Box padding={2}></Box>
+      </div>
+
+
+
+
 
 
     </Box>
@@ -50,3 +96,15 @@ function Skill() {
 export default Skill
 
 
+        //<Typography paddingTop={1} variant='h5'>ExpressJS - Udemy</Typography>
+        /*
+
+              <div className="progressBar">
+
+        <h4>ExpressJS - Udemy</h4>
+        <Typography paddingBottom={1} paddingTop= {1}>ExpressJS skills</Typography>
+        <ProgressBar now={40} />
+        <Box padding={2}></Box>
+      </div>
+
+      */
