35d60496766d269d8d9fb17d58ac8e17515e842f
BinaryAnxiety <713jiaearn@gmail.com>
2023-01-16 18:52:53 +0800

    MORE AESTHETICS

--- frontend/src/pages/explore/Individual.jsx
+++ frontend/src/pages/explore/Individual.jsx
@@ -18,7 +18,7 @@ function Individual() {
         days: "5 days ago",
         location: "Singapore",
         img: csitLogo, 
-        compensation: "$5,000 - $7,000 / mth",
+        compensation: "$5,000 - $7,500 / mth",
         rating: 5, 
         skills: "ReactJS, Javascript"
       }, 
