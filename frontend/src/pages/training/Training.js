6cad070ab046a1703d4cbd023b656b1ac96d1ff7
jklye <lyejinkai@gmail.com>
2023-01-16 15:19:10 +0800

    Added leaderboard feature on training page

---
+++ frontend/src/pages/training/Training.js
@@ -0,0 +1,12 @@
+import Board from './components/board';
+import './components/styles.css';
+
+function Training() {
+  return (
+    <div className="Training" id='main'>
+        <Board></Board>
+    </div>
+  );
+}
+
+export default Training;
